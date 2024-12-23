f11bde62d806fd681515b465a5869be6bf2a3a11
Hoon Choi <photiary@gmail.com>
2024-12-23 23:05:47 +0900

    vuetify 예제 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,13 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from '@/App.vue';
 import '@/assets/style.scss';
 import '@/mocks';
+import { setup } from '@storybook/vue3';
+import { createPinia } from 'pinia';
+import vuetify from '@/plugins/vuetify';
 
-const pinia = createPinia();
-createApp(App).use(pinia);
+setup((app) => {
+  const pinia = createPinia();
+  app.use(vuetify).use(pinia);
+});
 
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
