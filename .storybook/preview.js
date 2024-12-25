7ed01ad86093947e0831dacf3106d5a2e0cb77d3
Hoon Choi <photiary@gmail.com>
2024-12-25 16:02:53 +0900

    setup 함수 사용
    (cherry picked from commit 6a6995245d8c42ec915ec4cb8e7e1531d62541e0)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,13 @@
-import '@/styles/style.scss';
-import '@/mocks';
 import { setup } from '@storybook/vue3';
 import { createPinia } from 'pinia';
+import '@/styles/style.scss';
+import '@/mocks';
+import router from '@/router';
 import vuetify from '@/plugins/vuetify';
 
 const pinia = createPinia();
 setup((app) => {
-  app.use(vuetify).use(pinia);
+  app.use(vuetify).use(pinia).use(router);
 });
 
 /** @type { import('@storybook/vue3').Preview } */
